d914754f455baea08d14bd6c92ed4cb7e2cebc13
Alexander <sunchos89@mail.ru>
2017-03-06 19:49:28 +0200

    Try to draw 2 triangles next to each other using glDrawArrays by adding more vertices to your data

--- WindowGLFW/WindowGLFW.cpp
+++ WindowGLFW/WindowGLFW.cpp
@@ -93,30 +93,53 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	//GLfloat vertices[] = {
+	//	0.5f,  0.5f, 0.0f,  // Top Right
+	//	0.5f, -0.5f, 0.0f,  // Bottom Right
+	//	-0.5f, -0.5f, 0.0f,  // Bottom Left
+	//	-0.5f,  0.5f, 0.0f   // Top Left 
+	//};
+	//GLuint indices[] = {  // Note that we start from 0!
+	//	0, 1, 3,  // First Triangle
+	//	1, 2, 3   // Second Triangle
+	//};
 	GLfloat vertices[] = {
+		// first triangle
+		-0.5f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
+		0.0f, 0.0f, 0.0f,
+		// second triangle
+		0.f, 0.f, 0.0f,
 		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.5f, -0.5f, 0.0f
 	};
 
-	GLuint VBO, VAO;
-	glGenBuffers(1, &VBO);
+	GLuint VBO, VAO;//, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
 
 	// Bind Vertex Array.
 	glBindVertexArray(VAO);
 	// 0. Copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Copy EBO.
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 1. Linking Vertex Attributes pointers.
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	//! Unbind Vertex Array.
 	glBindVertexArray(0);
 
 	GLclampf red = 0.01f;
 	GLclampf green = 0.002f;
 	GLclampf blue = 0.003f;
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -136,9 +159,9 @@ int main()
 		// 2. Use our shader program when we want to render an object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
-
 
 		glfwSwapBuffers(window);
 
@@ -147,8 +170,10 @@ int main()
 		//blue += 0.003f;
 	}
 
+	//glDeleteVertexArrays(1, &EBO);
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 
